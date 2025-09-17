7267ad5f06d6985e47188808a13fb4c46bf848f6
Marcelo <marcelo.casanova@live.com>
2025-09-17 17:48:48 -0300

    Ajustes no arquivo de importação de dados

--- insert_db.py
+++ insert_db.py
@@ -25,13 +25,13 @@ def import_from_csv(file_path, tb_name):
         match tb_name:
             case 'tb_unidades':
                 try:
-                    df = pd.read_csv(file_path)
+                    df = pd.read_csv(file_path, encoding='latin-1')
             
                 except FileNotFoundError:
                     print(f"Erro: O arquivo {file_path} não foi encontrado.")
                     return
                 except Exception as e:
-                    print(f"Erro ao ler o arquivo .xlsx: {e}")
+                    print(f"Erro ao ler o arquivo .csv: {e}")
                     return
                 
                 for index, row in df.iterrows():
@@ -54,12 +54,78 @@ def import_from_csv(file_path, tb_name):
                 print("Importação concluída com sucesso.")
                 
             case 'tb_status':
-                pass
                 
-            case 'tb_tipos':
-                pass                
+                ############### IMPORTAÇÃO DE DADOS PARA A TABELA STATUS ####################
+                try:
+                    df = pd.read_csv(file_path, encoding='utf-8')
+            
+                except FileNotFoundError:
+                    print(f"Erro: O arquivo {file_path} não foi encontrado.")
+                    return
+                except Exception as e:
+                    print(f"Erro ao ler o arquivo .csv: {e}")
+                    return
+                
+                for index, row in df.iterrows():
+                    try:
+                        new_status = StatusIncidente(
+                            status = row['status'],
+                            desc_status = row['descricao']
+                        )
+                        db.session.add(new_status)
+                        print(f"Adicionando status: id={index}, Status={row['status']}, Descrição={row['descricao']}")
+                    except KeyError as e:
+                        print(f"Erro de importação na linha {index + 1}: Coluna {e.args[0]} não encontrada. Pulando linha")
+                        continue
+                    except Exception as e:
+                        print(f"Erro inesperado na linha {index + 1}: {e}. Pulando linha")
+                        continue
+                # Comitando todas as adições de uma vez
+                db.session.commit()
+                print("Importação concluída com sucesso.")
+                        
+            case 'tb_tipos_incidentes':
+                 
+                    ############### IMPORTAÇÃO DE DADOS PARA A TABELA TIPOS DE INCIDENTE ####################
+                    
+                    try:
+                        df = pd.read_csv(file_path, encoding='utf-8')
+                    except FileNotFoundError:
+                        print(f"Erro: O arquivo {file_path} não foi encontrado.")
+                        return
+                    except Exception as e:
+                        print(f"Erro ao ler o arquivo .csv: {e}")
+                        return
+                    
+                    for index, row in df.iterrows():
+                        try:
+                            new_tp_incidente = TipoIncidente(
+                                tipo_incidente = row['tipo_incidente'],
+                                desc_incidente = row['descricao']
+                            )
+                            db.session.add(new_tp_incidente)
+                            print(f"Adicionando Tipo de incidente: id={index}, Tipo Incidente={row['tipo_incidente']}, Descrição={row['descricao']}")
+                        except KeyError as e:
+                            print(f"Erro de importação na linha {index + 1}: Coluna {e.args[0]} não encontrada. Pulando linha")
+                            continue
+                        except Exception as e:
+                            print(f"Erro inesperado na linha {index + 1}: {e}. Pulando linha")
+                            continue
+                    # Comitando todas as adições de uma vez
+                    db.session.commit()
+                    print("Importação concluída com sucesso.")
             
             case 'tb_incidentes':
+                try:
+                    df = pd.read_excel(file_path, sheet_name='Planilha1')
+            
+                except FileNotFoundError:
+                    print(f"Erro: O arquivo {file_path} não foi encontrado.")
+                    return
+                except Exception as e:
+                    print(f"Erro ao ler o arquivo .xlsx: {e}")
+                    return
+                
                 for index, row in df.iterrows():
                     try:
                         new_incidente = Incidente(
@@ -72,7 +138,7 @@ def import_from_csv(file_path, tb_name):
                             cia = row['cia'],
                             description = row['description'],
                             start_date = row['start_date'],
-                            end_date = row['end_date'],
+                            end_date = row['end_date'] if pd.notna(row['end_date']) else None,
                             status_incident = row['status_incident'],
                             user_id = row['user_id']
                         )
@@ -91,57 +157,16 @@ def import_from_csv(file_path, tb_name):
             case _:
                 print("Tabela inválida.")            
         
-        ##############################################################################
-        ############### IMPORTAÇÃO DE DADOS PARA A TABELA STATUS ####################
-        ############################################################################## 
-        # for index, row in df.iterrows():
-        #     try:
-        #         new_status = StatusIncidente(
-        #             status = row['status'],
-        #             desc_status = row['descricao']
-        #         )
-        #         db.session.add(new_status)
-        #         print(f"Adicionando status: id={index}, Status={row['status']}, Descrição={row['descricao']}")
-        #     except KeyError as e:
-        #         print(f"Erro de importação na linha {index + 1}: Coluna {e.args[0]} não encontrada. Pulando linha")
-        #         continue
-        #     except Exception as e:
-        #         print(f"Erro inesperado na linha {index + 1}: {e}. Pulando linha")
-        #         continue
-        # # Comitando todas as adições de uma vez
-        # db.session.commit()
-        # print("Importação concluída com sucesso.")
         
-        ##############################################################################
-        ############### IMPORTAÇÃO DE DADOS PARA A TABELA TIPOS DE INCIDENTE ####################
-        ############################################################################## 
-        # for index, row in df.iterrows():
-        #     try:
-        #         new_tp_incidente = TipoIncidente(
-        #             tipo_incidente = row['tipo_incidente'],
-        #             desc_incidente = row['descricao']
-        #         )
-        #         db.session.add(new_tp_incidente)
-        #         print(f"Adicionando Tipo de incidente: id={index}, Tipo Incidente={row['tipo_incidente']}, Descrição={row['descricao']}")
-        #     except KeyError as e:
-        #         print(f"Erro de importação na linha {index + 1}: Coluna {e.args[0]} não encontrada. Pulando linha")
-        #         continue
-        #     except Exception as e:
-        #         print(f"Erro inesperado na linha {index + 1}: {e}. Pulando linha")
-        #         continue
-        # # Comitando todas as adições de uma vez
-        # db.session.commit()
-        # print("Importação concluída com sucesso.")
+        
+       
         
         
         
-        ##############################################################################
-        ############### IMPORTAÇÃO DE DADOS PARA A TABELA INCIDENTE ####################
-        ############################################################################## 
-        
+       
         
 
 if __name__ == '__main__':
-    csv_file_path = 'tb_unidades.csv' 
+    csv_file_path = 'tb_unidades.csv'
     import_from_csv(csv_file_path, "tb_unidades")        
             
